514efe51f64d1f25c8a200e5d4f1af3ac2423b3c
Your Name <you@example.com>
2024-02-15 14:35:21 -0500

    adiciones

---
+++ core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1&c3qe*r9^%ht7ypg4p8l6_ey&%f^wah6n5-%t2rkc0mzn1=g-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
